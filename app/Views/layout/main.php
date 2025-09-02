8468b0dcec3a8446475b483a66c2694232b8f0db
Pramtoxz <pramuditometra@gmail.com>
2025-09-02 17:53:18 +0700

    title mpz

--- app/Views/layout/main.php
+++ app/Views/layout/main.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PROMEDICO <?= $title ?? 'DENTAL' ?></title>
+    <title>MPZ <?= $title ?? 'FKUNAND' ?></title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
